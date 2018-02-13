fac5aae3c24682a73b6303dd3bb1e4c4af1e2117
ciagar23 <fredowinz23@gmail.com>
2018-02-13 23:16:01 +0800

    Created admin layout

--- include/template_login.php
+++ include/template_login.php
@@ -1,8 +1,3 @@
-
-<h1>header </h1>
-
 <?php
 include $content;
 ?>
-
-<h1>footer </h1>
--- user/login.php
+++ user/login.php
@@ -1,14 +1,118 @@
 <?php
 $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 ?>
-<?=$error?>
-<br>
-<form action="process.php?action=login" method="POST">
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Teamire - Insert System Name</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-					<input class="form-control" placeholder="User Name" name="username" required> <br>
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="../include/assets/images/favicon.ico">
 
-                    <input class="form-control" placeholder="Password" name="password" type="password" required>
-               <br>
-                <button type="submit" >Login</button>
+        <!-- App css -->
+        <link href="../include/../include/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../include/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="../include/assets/css/metismenu.min.css" rel="stylesheet" type="text/css" />
+        <link href="../include/assets/css/style.css" rel="stylesheet" type="text/css" />
 
-</form>
+        <script src="../include/assets/js/modernizr.min.js"></script>
+
+    </head>
+
+
+    <body class="bg-accpunt-pages">
+
+        <!-- HOME -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+
+                        <div class="wrapper-page">
+
+                            <div class="account-pages">
+                                <div class="account-box">
+                                    <div class="account-logo-box">
+                                        <h2 class="text-uppercase text-center">
+                                            <a href="index.html" class="text-success">
+                                                <span>Teamire</span>
+                                            </a>
+                                        </h2>
+                                        <h5 class="text-uppercase font-bold m-b-5 m-t-50">Sign In</h5>
+                                        <p class="m-b-0">
+																				<?=$error?></p>
+                                    </div>
+                                    <div class="account-content">
+                                        <form class="form-horizontal" action="process.php?action=login" method="POST">
+
+                                            <div class="form-group m-b-20 row">
+                                                <div class="col-12">
+                                                    <label for="emailaddress">Username</label>
+                                                    <input class="form-control" type="text" id="emailaddress" name="username" required="" placeholder="">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row m-b-20">
+                                                <div class="col-12">
+                                                    <a href="page-recoverpw.html" class="text-muted pull-right"><small>Forgot your password?</small></a>
+                                                    <label for="password">Password</label>
+                                                    <input class="form-control" type="password" required="" id="password" name="password" placeholder="">
+                                                </div>
+                                            </div>
+
+
+                                            <div class="form-group row text-center m-t-10">
+                                                <div class="col-12">
+                                                    <button class="btn btn-md btn-block btn-primary waves-effect waves-light" type="submit">Sign In</button>
+                                                </div>
+                                            </div>
+
+                                        </form>
+
+
+                                        <div class="row m-t-50">
+                                            <div class="col-sm-12 text-center">
+                                                <p class="text-muted">Don't have an account? <a href="page-register.html" class="text-dark m-l-5"><b>Sign Up</b></a></p>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end card-box-->
+
+
+                        </div>
+                        <!-- end wrapper -->
+
+                    </div>
+                </div>
+            </div>
+          </section>
+          <!-- END HOME -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="../include/assets/js/jquery.min.js"></script>
+        <script src="../include/assets/js/tether.min.js"></script><!-- Tether for Bootstrap -->
+        <script src="../include/assets/js/bootstrap.min.js"></script>
+        <script src="../include/assets/js/metisMenu.min.js"></script>
+        <script src="../include/assets/js/waves.js"></script>
+        <script src="../include/assets/js/jquery.slimscroll.js"></script>
+
+        <!-- App js -->
+        <script src="../include/assets/js/jquery.core.js"></script>
+        <script src="../include/assets/js/jquery.app.js"></script>
+
+    </body>
+</html>
