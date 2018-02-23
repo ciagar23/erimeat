42028a3e7b69e3bd19d491c515bc6140a433c1b8
ciagar23 <fredowinz23@gmail.com>
2018-02-23 22:09:12 +0800

    Added new databse

--- db_erimeat.sql
+++ db_erimeat.sql
@@ -1,8 +1,7 @@
 ﻿# Host: localhost  (Version 5.5.5-10.1.30-MariaDB)
-# Date: 2018-02-23 21:54:05
-# Generator: MySQL-Front 5.4  (Build 1.40)
+# Date: 2018-02-23 22:08:56
+# Generator: MySQL-Front 6.0  (Build 2.20)
 
-/*!40101 SET NAMES utf8 */;
 
 #
 # Structure for table "application"
@@ -37,12 +36,13 @@ CREATE TABLE `candidate` (
   `email` varchar(100) DEFAULT NULL,
   `contactNumber` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "candidate"
 #
 
+INSERT INTO `candidate` VALUES (1,'1','honda@city.com','098765432112'),(2,'0','ba@bi.com','123456789009'),(3,'jackDaniel','jack@daniel.com','12345678909'),(4,'bibi','bi@bi.com','123456778909'),(5,'ehogirl','eho@girl.com','1234676');
 
 #
 # Structure for table "company"
@@ -59,12 +59,35 @@ CREATE TABLE `company` (
   `contactNumber` varchar(100) DEFAULT NULL,
   `address` text,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "company"
 #
 
+INSERT INTO `company` VALUES (1,'0','publiq','publq','publiq@publiq.com','sam sedayon','09876543112','ramos billboard'),(2,'s','samiguel','samgiel','san@miguel.com','miguel','12345678909','bacolod'),(3,'b','bababol','bababol','ba@ba.com','bababe','1234567890','bacolo'),(4,'company','company','company','com@pany.com','123456','123456','bacolod');
+
+#
+# Structure for table "dtr"
+#
+
+DROP TABLE IF EXISTS `dtr`;
+CREATE TABLE `dtr` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_name` varchar(255) DEFAULT NULL,
+  `dtr_date` varchar(255) DEFAULT NULL,
+  `checkIn` varchar(255) DEFAULT NULL,
+  `breakOut` varchar(255) DEFAULT NULL,
+  `breakIn` varchar(255) DEFAULT NULL,
+  `checkOut` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+#
+# Data for table "dtr"
+#
+
+INSERT INTO `dtr` VALUES (1,'dfdsf dgfd','2018-02-24','06:49:49am',NULL,NULL,NULL);
 
 #
 # Structure for table "employee"
@@ -132,10 +155,10 @@ CREATE TABLE `hr` (
 DROP TABLE IF EXISTS `job`;
 CREATE TABLE `job` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `refNum` varchar(25) DEFAULT NULL,
   `jobFunctionId` int(11) DEFAULT NULL,
   `positionTypeId` int(11) DEFAULT NULL,
   `position` varchar(255) DEFAULT NULL,
-  `owner` varchar(10) DEFAULT NULL,
   `company` varchar(255) DEFAULT NULL,
   `workEmail` varchar(100) DEFAULT NULL,
   `JobTitle` varchar(255) DEFAULT NULL,
@@ -146,14 +169,16 @@ CREATE TABLE `job` (
   `comment` varchar(255) DEFAULT NULL,
   `createDate` datetime DEFAULT NULL,
   `isApproved` varchar(1) DEFAULT '0',
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "job"
 #
 
-INSERT INTO `job` VALUES (2,0,0,'sample lang','admin','jjjjjjjjjj','jjjjjjjjjjj','jjjjjjjjjjjjjj','jjjjjjjjjjjjj','jjjjjjjjjjjjjjj','sample address','1-2 years','jjjjjjjjjjjj','2018-02-19 20:11:47','0'),(3,0,0,'developer','admin','hyndrance','a@a.com','developer','123456','6100',NULL,'1','sdfsfd','2018-02-19 22:29:35','0');
+INSERT INTO `job` VALUES (8,NULL,0,0,'yyoooooo','ooooooo','oooo','oooo','oooo','ooooo',NULL,'oooo','yyooo','2018-02-23 20:36:44','0','ooooo','ooooooo'),(9,NULL,0,0,'yyoooooo','ooooooo','oooo','oooo','oooo','ooooo',NULL,'oooo','yyooo','2018-02-23 20:37:43','0','ooooo','ooooooo'),(10,NULL,0,0,'yyoooooo','ooooooo','oooo','oooo','oooo','ooooo',NULL,'oooo','yyooo','2018-02-23 20:38:17','0','ooooo','ooooooo'),(11,NULL,0,0,'yyoooooo','ooooooo','oooo','oooo','oooo','ooooo',NULL,'oooo','yyooo','2018-02-23 20:38:57','0','ooooo','ooooooo'),(12,NULL,5,0,'yyoooooo','ooooooo','oooo','oooo','oooo','ooooo',NULL,'oooo','yyooo','2018-02-23 20:39:26','0','ooooo','ooooooo'),(13,NULL,3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:40:54','0','hkj','hkj'),(14,NULL,3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:42:27','0','hkj','hkj'),(15,NULL,3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:43:01','0','hkj','hkj'),(16,'1519389842',3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:44:02','0','hkj','hkj'),(17,'1519389893',3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:44:52','0','hkj','hkj'),(18,'1519389904',3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:45:03','0','hkj','hkj'),(19,'1519389925',3,1,'kjhkj','hkj','hkj','hkjhkjh','kjhkjh','kjh',NULL,'kjhkj','hkjh','2018-02-23 20:45:24','0','hkj','hkj'),(20,'J1519389960',1,1,'','','','','','',NULL,'','','2018-02-23 20:45:59','0','',''),(21,'J',1,1,'','','','','','',NULL,'','','2018-02-23 20:46:19','0','',''),(22,'J1519389992',1,1,'','','','','','',NULL,'','','2018-02-23 20:46:32','0','',''),(23,'1519390202',1,1,'hgihgiig','iugiugiu','iugiug','iugygyg','ytvfytvty','vytvyt',NULL,'vytvyt','vytvy','2018-02-23 20:50:02','0','giug','iugiug'),(24,'1519390847',1,1,'hgihgiig','iugiugiu','iugiug','iugygyg','ytvfytvty','vytvyt',NULL,'vytvyt','vytvy','2018-02-23 21:00:46','0','giug','iugiug'),(25,'1519390887',1,1,'hgihgiig','iugiugiu','iugiug','iugygyg','ytvfytvty','vytvyt',NULL,'vytvyt','vytvy','2018-02-23 21:01:27','0','giug','iugiug'),(26,'1519390999',1,1,'hgihgiig','iugiugiu','iugiug','iugygyg','ytvfytvty','vytvyt',NULL,'vytvyt','vytvy','2018-02-23 21:03:18','0','giug','iugiug'),(27,'1519391073',1,1,'jjj','yugu','oiuiou','iouoi','rdtrd','ccc',NULL,'deee','trdrtdtrd','2018-02-23 21:04:32','0','yfyufu','yufuf'),(28,'1519391157',1,1,'kjhkjh','kjhkjh','kjhkjh','kjhkjh','kjhjkh','kjhkjh','kjhkjhk','kjhg','yugfuyfu','2018-02-23 21:05:56','0','kjh','kjhkjhkj'),(29,'9391932',1,1,'yyyyyyyyyy','yy','hjih','iiuh','iuhiuh','iuhiuhiu','yyy','iuhiuhiuh','uhi','2018-02-23 21:18:51','0','y','yyy'),(30,'391954',1,1,'yyyyyyyyyy','yy','hjih','iiuh','iuhiuh','iuhiuhiu','yyy','iuhiuhiuh','uhi','2018-02-23 21:19:13','0','y','yyy');
 
 #
 # Structure for table "job_function"
@@ -211,13 +236,13 @@ CREATE TABLE `resume` (
   `uploadedResume` varchar(100) DEFAULT NULL,
   `createDate` datetime DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "resume"
 #
 
-INSERT INTO `resume` VALUES (3,0,0,'u','u','u','u','u','u','u','u','u','sleepnot','1519050194.txt','2018-02-19 22:23:13'),(4,0,0,'u','u','u','u','u','u','u','u','u','sleepnot','1519050322.txt','2018-02-19 22:25:22'),(5,0,4,'u','u','u','u','u','u','u','u','u','sleepnot','1519050333.txt','2018-02-19 22:25:33'),(6,0,1,'dale','torre','dale.torre@tekinbox.com','1235','bacolod','baclod','city','state','6100','admin','1519050672.txt','2018-02-19 22:31:11'),(7,0,4,'adf','adsf','a@a.com','123123','adsf','asdf','sadf','adsf','123','employer','1519130918.png','2018-02-20 20:48:38');
+INSERT INTO `resume` VALUES (3,0,0,'u','u','u','u','u','u','u','u','u','sleepnot','1519050194.txt','2018-02-19 22:23:13'),(4,0,0,'u','u','u','u','u','u','u','u','u','sleepnot','1519050322.txt','2018-02-19 22:25:22'),(5,0,4,'u','u','u','u','u','u','u','u','u','sleepnot','1519050333.txt','2018-02-19 22:25:33'),(6,0,1,'dale','torre','dale.torre@tekinbox.com','1235','bacolod','baclod','city','state','6100','admin','1519050672.txt','2018-02-19 22:31:11');
 
 #
 # Structure for table "timesheet"
@@ -248,16 +273,16 @@ CREATE TABLE `timesheet` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `username` varchar(10) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
   `firstName` varchar(50) DEFAULT NULL,
   `lastName` varchar(50) DEFAULT NULL,
   `level` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "user"
 #
 
-INSERT INTO `user` VALUES (15,'publiq_12345','12345','publiq','cafe','company'),(16,'job_12345','temppassword','dale','torre','employee');
+INSERT INTO `user` VALUES (1,'employee','123456','Winifredo','Garcia','a'),(2,'employer','123456','pati ang iban ma islan','username','a'),(3,'admin','admin','dfdsf','dgfd','admin'),(4,'hr','12345','sam','sedayon','hr'),(5,'honda','12345','honda','city','employee'),(6,'publiq','12345','sam','sedayon','employer'),(7,'babi','12345','ba','bi','employee'),(8,'biba','12345','bi','ba','employee'),(9,'jackDaniel','12345','jack','daniel','employee'),(10,'sanMig','12345','san','mig','employer'),(11,'bibi','12345','bi','refNum','employee'),(12,'baba','12345','ba','ba','employer'),(13,'ehogirl','12345','eho','girl','employee'),(14,'company','12345','com','pany','employer');
