c32a8581bef88be0e4a9d2149e7ed100f7edd9fd
makcintosh <rgmak12@gmail.com>
2018-02-24 21:09:00 -0800

    Updated timesheet // The message $message = "Line 1\r\nLine 2\r\nLine 3";
    // In case any of our lines are larger than 70 characters, we should use wordwrap()
    $message = wordwrap($message, 70, "\r\n");
    
    // Send
    mail('caffeinated@example.com', 'My Subject', $message);
    ?>

--- timesheet/list.php
+++ timesheet/list.php
@@ -1,64 +1,30 @@
+
 <div class="card-box">
     <div class="row">
 
        
                             <div class="col-sm-12">
-                            <div class="pull-right">
+                            <center>
                             <form method="POST">
-  <button type="submit" class="btn btn-primary" name="checkIn"><span class="fa fa-clock-o"></span> Check In</button> 
-    <input type="hidden" name="employee_name" value="<?=$profile->firstName;?> <?=$profile->lastName;?>">
+                               
+  <button type="submit" class="btn btn-primary" name="checkIn"><span class="fa fa-clock-o"></span> Check In</button>
+
+   <input type="hidden" name="employee_name" value="John Doe  ">
   <input type="hidden" name="dtr_date" value="<?php echo date('Y-m-d')?>">
-  <input type="hidden" name="checkIn" value="<?php echo date('h:i:sa')?>"">
+  <input type="hidden" name="checkIn" value="<?php echo date('h:i:sa')?>">
 </form>
-
-
-
-
-
-
-</div>
-<br>
-<br>
-
-     
-                                <div class="card-box table-responsive">
-                                    <h4 class="m-t-0 header-title"><b>List of Time Submitted</b></h4>
-                                   
-
-                                         <table id="datatable-buttons" class="table table-striped table-bordered">
-                                        <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Date</th>
-                                        
-                                            <th>Check In</th>
-                                            <th>Break Out</th>
-                                            <th>Break In</th>
-                                            <th>Check Out</th>
-
-                                          <th>Action</th>
-                                        </tr>
-                                        </thead>
-
-
-                                        <tbody>
-                                    <?php
+<?php
                  $conn = new PDO('mysql:host=localhost; dbname=db_erimeat','root', ''); 
-                $result = $conn->prepare("SELECT * FROM dtr ORDER BY employee_name ASC");
+                $result = $conn->prepare("SELECT * FROM dtr ORDER BY dtr_date ASC LIMIT 1");
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
                 $id=$row['Id'];
               ?>
-              <tr>
-                <td> <?php echo $row ['employee_name']; ?></td>
-                <td> <?php echo $row ['dtr_date']; ?></td>
-                <td> <?php echo $row ['checkIn']; ?></td>
-                <td> <?php echo $row ['breakOut']; ?></td>
-                <td> <?php echo $row ['breakIn']; ?></td>
 
-                <td> <?php echo $row ['checkOut']; ?></td>
-                <td>
-                <?php
+<br>
+
+
+<?php
                 if ($row ['breakOut']==TRUE) {
                   echo ' <button  class="btn btn-default"  disabled><span class="fa fa-clock-o"></span> Break out</button> |';
                 }
@@ -103,20 +69,78 @@
                 }
                   else if ($row ['checkIn']==TRUE && $row ['breakOut']==TRUE && $row['breakIn']==TRUE &&  $row ['checkOut']==FALSE) {
                     ?>
-                       <a href="checkout.php<?php echo '?id='.$id; ?>" class="btn btn-danger" name="breakOut"><span class="fa fa-clock-o"></span> Check out</a> 
+                       <a href="checkout.php<?php echo '?id='.$id; ?>" class="btn btn-danger" name="breakOut"><span class="fa fa-clock-o"></span> Check out</a> |
 
                     <?php
    
                 }
 
                    else{
-                  echo ' <button  class="btn btn-default"  disabled><span class="fa fa-clock-o"></span> Check out</button>';
+                  echo ' <button  class="btn btn-default"  disabled><span class="fa fa-clock-o"></span> Check out</button> |';
                 }
                 ?>
                 
 
 
+  <a href="checkout.php<?php echo '?id='.$id; ?>" class="btn btn-info" name="breakOut"><span class="fa fa-clock-o"></span> Remarks</a>
 
+
+
+</center>
+<br>
+<br>
+
+     
+                                <div class="card-box table-responsive">
+                                 
+
+                                         <table class="table table-striped table-bordered">
+                                        <thead>
+                                        <tr>
+                                         <!-- <th>Name</th> -->
+                                            <th>Date</th>
+                                          
+                                            <th>Check In</th>
+                                            <th>Break Out</th>
+                                            <th>Break In</th>
+                                            <th>Check Out</th>
+
+                                          <th>Status</th>
+                                        </tr>
+                                        </thead>
+
+
+                                        <tbody>
+                                 
+              <tr>
+              <!-- <td> <?php echo $row ['employee_name']; ?></td> -->
+                <td> <?php echo date('Y-m-d')?></td>
+                <td> <?php echo date('h:i:sa')?></td>
+                <td> <?php echo date('h:i:sa')?></td>
+                <td> <?php echo date('h:i:sa')?></td>
+
+                <td> <?php echo date('h:i:sa')?></td>
+                <td>
+
+                <?php
+                if ($row ['checkIn']==TRUE && $row ['breakOut']==FALSE && $row['breakIn']==FALSE &&  $row ['checkOut']==FALSE) {
+                  echo '<span class="btn btn-success">Checked In</span>';
+                }
+
+                else if($row ['checkIn']==TRUE && $row ['breakOut']==TRUE && $row['breakIn']==FALSE &&  $row ['checkOut']==FALSE){
+                    echo '<span class="btn btn-warning">Break Out</span>';
+                }
+
+                else if($row ['checkIn']==TRUE && $row ['breakOut']==TRUE && $row['breakIn']==TRUE &&  $row ['checkOut']==FALSE){
+                    echo '<span class="btn btn-info">Break In</span>';
+                }
+
+                 else if($row ['checkIn']==TRUE && $row ['breakOut']==TRUE && $row['breakIn']==TRUE &&  $row ['checkOut']==TRUE){
+                    echo '<span class="btn btn-danger">Checked Out</span>';
+                }
+               ?>
+
+               
 
                 
                
