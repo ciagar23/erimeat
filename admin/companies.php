77a495afb3a87bb19c8432065c02f42fa30bcae1
acknolodgia <torredale1014@gmail.com>
2018-02-28 01:33:32 +0800

    Fix accounts and company list

--- admin/companies.php
+++ admin/companies.php
@@ -1,52 +1,37 @@
 <?php
 $s = (isset($_GET['s']) && $_GET['s'] != '') ? $_GET['s'] : '';
-
-$obj = new Job;
-
+$obj = new Company;
 ?>
 
 <div class="card-box">
-
-
-
-     <div class="row">
-                            <div class="col-sm-12">
-                          
-                                <div class="card-box table-responsive">
-                                    <h4 class="m-t-0 header-title"><b>List of Companies</b></h4>
-                                   
-
-                                    <table id="datatable" class="table table-striped table-bordered">
-                                        <thead>
-                                        <tr>
-                                            <th>Company Name</th>
-                                            
-                                              <th>Action</th>
-                                            
-                                        </tr>
-                                        </thead>
-
-
-                                        <tbody>
-                                       <?php foreach($obj->readList($s) as $row) {
-  if ($row->isApproved==1){
-?>
-                                        <tr>
-                                            <td><?=$row->company;?> </td>
-                                            
-                                            <td>
-                                                 <a href="?view=companyDetail&Id=<?=$row->Id;?>"  class=" btn btn-success btn-xs tooltips" title="Click To Edit"><span class="fa fa-eye"></span> View Details</a>
-                                            </td>
-                                         
-                                        </tr>
-                                        <?php
-                                            }
-                                                }
-                                        ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card-box table-responsive">
+        <h4 class="m-t-0 header-title"><b>List of Companies</b></h4>
+        <table id="datatable" class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Company Name</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($obj->readList($s) as $row) {
+              if ($row->isApproved==1){
+            ?>
+            <tr>
+              <td><?=$row->name;?> </td>
+              <td>
+                <a href="?view=companyDetail&Id=<?=$row->Id;?>"  class=" btn btn-success btn-xs tooltips" title="Click To Edit"><span class="fa fa-eye"></span> View Details</a>
+              </td>
+            </tr>
+            <?php
+              }
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
