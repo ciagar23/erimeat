631cab052c6906dc3272d5acc2ac21ecd1c95aef
acknolodgia <torredale1014@gmail.com>
2018-02-28 14:47:48 +0800

    Create login page for client and candidate

---
+++ home/logins.php
@@ -0,0 +1,32 @@
+<div align="center"><strong>Timesheets and Login</strong></div>
+</br></br>
+<div align="center">
+  Follow the links below to access the online timesheet system or candidate My Dashboard. To sign in, enter the login details that were</br>
+  supplied to you by Robert Half. If you need assistance with either system, please contact your staffing manager.
+</div>
+</br></br>
+<div align="center">
+  <div class="col-md-6">
+    <div>
+      <strong>
+        Client Login
+      </strong>
+    </div>
+    <div>
+      <button><a href="../company/?view=login">Timekeeping</button>
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div>
+      <strong>
+        Candidate Login
+      </strong>
+    </div>
+    <div>
+      <button><a href="../employee/?view=login">Timekeeping</a></button>
+      </br></br>
+      <button>My Dashboard</button>
+    </div>
+  </div>
+</div>
