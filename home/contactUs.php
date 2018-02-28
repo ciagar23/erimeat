676e1642281317ec611e513281d6e50545a96593
designkid <robertcdc21@gmail.com>
2018-02-28 19:32:28 +0800

    Modify Contact us page

--- home/contactUs.php
+++ home/contactUs.php
@@ -1,6 +1,34 @@
 <div class="container-fluid">
-  <div class="card-box">
-    <h4 class="text-uppercase font-18 text-primary text-center">Lorem Ipsum</h4>
+  <h2 class="text-center m-t-30 m-b-30">Contact Us</h2>
+  <h3 class="text-center m-t-30 m-b-30">Enter your city or ZIP code to find an office near you.</h3>
+      <div class="col-md-8 center-page">
+          <div class="input-group m-t-5">
+              <input type="text" id="" name="" class="form-control" style="height: 70px; border-color: #727B80;" placeholder="Job Title, Skills or Keywords">
+              <span class="input-group-btn">
+                  <button type="button" class="btn stepy-finish waves-light btn-primary"><i class="fa fa-search m-r-5"></i> Search Jobs</button>
+              </span>
+          </div>
+      </div>
+      <div class="clearfix"></div>
+      <div class="row">
+        <div class="col-lg-1">&nbsp;</div>
+        <div class="col-12 col-lg-4">
+          <h2 class="display3 text-center mt30 mb25">Employers</h2>
+          <p class="subtitle text-center mb20">Looking for the right candidate?</p>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+        </div>
+        <div class="col-lg-2">&nbsp;</div>
+        <div class="col-12 col-lg-4">
+          <h2 class="display3 text-center mt30 mb25">Employers</h2>
+          <p class="subtitle text-center mb20">Looking for the right candidate?</p>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+          <div class="text-center mt0"><a class="btn btn-primary rh-button-small rh-button__full-width-all tabBtn" href="/hiring-request-form" role="button">REQUEST TALENT</a></div>
+        </div>
+        <div class="col-lg-1">&nbsp;</div>
+      </div>
     <div class="col-md-6">
       <!-- Contact form -->
       <div class="col-md-10">
@@ -68,8 +96,6 @@
       </div> <!-- end col -->
     </div>
     <div class="clearfix"></div>
-
-  </div>
   <div class="card-box">
     <div class="row">
         <div class="col-md-8 center-page">
