21ee29864f0b3e88a6327fec4395887c480b8212
designkid <robertcdc21@gmail.com>
2018-02-28 16:53:20 +0800

    Modify submit resume form

--- home/submitResume.php
+++ home/submitResume.php
@@ -6,12 +6,11 @@ $pt->readList();
 ?>
 <div class="row">
     <div class="col-md-12">
-        <div class="card-box">
-
+      <h2 class="text-center m-t-30 m-b-30">Submit Resume</h2>
+      <div class="jumbotron center-page" style="width: 1140px;">
             <form id="default-wizard" action="process.php?action=submitResume" method="POST" enctype="multipart/form-data">
-                    <legend>Submit Resume</legend>
                     <div class="row m-t-20">
-                        <div class="col-sm-6">
+                        <div class="col-sm-7 center-page">
 
                               <div class="form-group">
                                   <label for="firstname">Job Function</label>
@@ -22,15 +21,21 @@ $pt->readList();
                                   </select>
                               </div>
 
+                              <div class="p-r-10 w-50-p pull-left">
                               <div class="form-group">
                                   <label for="username">First Name</label>
                                   <input type="text" class="form-control" name="firstName" placeholder="">
                               </div>
+                              </div>
 
+                              <div class="p-l-10 w-50-p pull-left">
                               <div class="form-group">
                                   <label for="username">Last Name</label>
                                   <input type="text" class="form-control" name="lastName" placeholder="">
                               </div>
+                              </div>
+
+                              <div class="clearfix"></div>
 
                               <div class="form-group">
                                   <label for="username">Email</label>
@@ -41,10 +46,6 @@ $pt->readList();
                                   <label for="username">Phone Number</label>
                                   <input type="text" class="form-control" name="phoneNumber" placeholder="">
                               </div>
-
-                        </div>
-
-                        <div class="col-sm-6">
 
 
                           <div class="form-group">
@@ -57,32 +58,42 @@ $pt->readList();
                               <input type="text" class="form-control" name="address2" placeholder="">
                           </div>
 
-                          <div class="form-group">
+                          <div>
+                          <div class="form-group w-33-p pull-left p-r-10">
                               <label for="username">City</label>
                               <input type="text" class="form-control" name="city" placeholder="">
                           </div>
 
-                          <div class="form-group">
+                          <div class="form-group w-33-p pull-left">
                               <label for="username">State</label>
                               <input type="text" class="form-control" name="state" placeholder="">
                           </div>
 
-                          <div class="form-group">
+                          <div class="form-group w-33-p pull-right">
                               <label for="username">Postal Code</label>
                               <input type="text" class="form-control" name="zipCode" placeholder="">
                           </div>
-
-                          <div class="form-group">
-                              <label for="username">Upload Resume</label>
-                                  <input type="file"  class="form-control" name="upload_file" required/>
                           </div>
-
+                          <div class="text-center"><h3>Attach Resume</h3>
+                          <p>Choose one of the following options:</p></div>
+                          <div class="col-sm-3">
+                            <div class="pull-right text-center form-icon">
+                              <i class="fa fa-file-text-o fa-2x text-white p-t-10"></i>
+                            </div>
+                          </div>
+                          <div class="col-md-8">
+                          <h4>Upload Your Resume</h4>
+                          <p>Supported File Types: pdf, txt, .doc, .docx</p>
                         </div>
+                          <div class="form-group">
+                              <input type="file"  class="form-control" name="upload_file" required/>
+                          </div>
                     </div>
-                <button type="submit" class="btn btn-primary stepy-finish">Submit</button>
+                  <div class="text-center m-t-30">
+                      <button type="submit" class="btn btn-primary stepy-finish"> SUBMIT RESUME </button>
+                  </div>
             </form>
-
-        </div>
+          </div>
     </div>
 </div>
 
--- include/template.php
+++ include/template.php
@@ -6,65 +6,10 @@
     <body>
         <!-- Navigation Bar-->
         <header id="topnav">
-            <div class="topbar-main">
-                <div class="container">
-                    <!-- Logo container-->
-                    <div class="logo col-md-6">
-                        <!-- Text Logo -->
-                        <!--<a href="index.html" class="logo">-->
-                        <!--Adminox-->
-                        <!--</a>-->
-                        <!-- Image Logo -->
-                        <a href="index.php" class="logo m-t-5 m-l-15">
-                          <img src="../include/assets/images/teamire-logo.png">
-                        </a>
-                    </div>
-
-                    <div class="navigation-menu col-md-6 " style="padding: 40px;">
-                      <div class="pull-right">
-                        <li>
-                          <a class="m-l-5 m-r-15" href="extras-pricing.html">Investor Center</a>
-                          <a class="m-l-5 m-r-15" href="extras-pricing.html">Timesheets</a>
-                          <a class="m-l-5 m-r-15" href="../home/?view=contactUs">Contact Us</a>
-                          <a class="m-l-5 m-r-15" href="extras-faq.html">Request Staff</a>
-                        </li>
-                    </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <!-- End Logo container-->
-
-                    <div class="menu-extras topbar-custom">
-
-                        <ul class="list-inline float-right mb-0">
-
-                            <li class="menu-item list-inline-item">
-                                <!-- Mobile menu toggle-->
-                                <a class="navbar-toggle nav-link">
-                                    <div class="lines">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </a>
-                                <!-- End mobile menu toggle-->
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- end menu-extras -->
-                    <div class="clearfix"></div>
-                  </div>
-                </div>
-
-                    <div class="navbar-custom">
-                        <div class="container">
-                            <?php
-                              include 'navVisitor.php';
-                            ?>
-                        </div> <!-- end container -->
-                    </div> <!-- end navbar-custom -->
-                    <div class="clearfix"></div>
-                </div> <!-- end container -->
-            </div>
+              <?php
+                include 'navVisitor.php';
+              ?>
+              <div class="clearfix"></div>
             <!-- end topbar-main -->
 
         </header>
