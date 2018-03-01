73be6b9ef31c785d9f0777c57fbb208d42893db0
makcintosh <rgmak12@gmail.com>
2018-03-01 15:48:19 -0800

    login and copyright footer design

--- employee/login.php
+++ employee/login.php
@@ -22,16 +22,18 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 
         <script src="../include/assets/js/modernizr.min.js"></script>
 
+       
     </head>
 
 
     <body >
-    <div style="border-bottom:5px solid #3399cc;">
-<img src="../include/assets/images/branding.jpg" >
+    <div id="sticky_header">
+       <a href="../home/index.php"><img src="../include/assets/images/branding.jpg" ></a>
 
       </div>
         <!-- HOME -->
-        <section>
+
+        <section >
            
                 <div class="row">
                     <div class="col-md-12">
@@ -40,12 +42,12 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 
                             <div class="account-pages">
                               <div class="login-left-box" >
-                                <div class="account-box" style="background-color: #3399cc;box-shadow: 5px 10px #888888;border-style: solid;border-color: #3399cc;">
+                                <div class="account-box" style="">
                                     <div class="account-logo-box">
                                      
                                         <h2 class="text-uppercase text-center" >
                                             <a href="index.html" class="login-font">
-                                                <span style="color:#fff;">Enter your login information to access our Time Reporting and Approval System</span>
+                                                <span >Enter your login information to access our Time Reporting and Approval System</span>
                                             </a>
                                         </h2>
                                         <p class="m-b-0">
@@ -56,14 +58,14 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 
                                             <div class="form-group m-b-0 row">
                                                 <div class="col-12">
-                                                    <label for="emailaddress" style="color:#fff;">Username</label>
+                                                    <label for="emailaddress">Username</label>
                                                     <input class="form-control" type="text" id="emailaddress" name="username" required="" placeholder="">
                                                 </div>
                                             </div>
 
                                             <div class="form-group row m-b-5">
                                                 <div class="col-12">
-                                                    <label for="password" style="color:#fff;">Password</label>
+                                                    <label for="password" >Password</label>
                                                     <input class="form-control" type="password" required="" id="password" name="password" placeholder="">
                                                 </div>
                                             </div>
@@ -78,7 +80,7 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
                                                 
                                             </div>
                                             <center>
-                                             <a href="page-recoverpw.html" class="text-muted" ><small style="color:#fff;">Don't recall your account information?</small></a>
+                                             <a href="page-recoverpw.html" class="text-muted" ><small>Don't recall your account information?</small></a>
                                              </center>
                                         </form>
 
@@ -87,9 +89,24 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 
                                     </div>
 
+
                                 </div>
                                  
+                                 
+                                         <footer class="footer-default">
+                                            <div class="container">
+                                                <div class="row">
+                                                    <div class="col-12 text-left">
+                                                    <br>
+                                                    <a href="">Terms of user</a> | <a href="">Privacy Policy</a> | <a href=""> Customer Service</a> <br>
+                                                        2018 © Teamire All Rights Reserved
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </footer>
+                                 
                               </div>
+                            
                         
                      
                         <!-- end wrapper -->
@@ -102,6 +119,8 @@ $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
             </div>
 
           </section>
+         
+
 
           
 
