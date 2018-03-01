9dd4edab642dc0fc346dff523bf4e2808cb6fdd7
designkid <robertcdc21@gmail.com>
2018-03-01 17:45:01 +0800

    Modify services

--- home/services.php
+++ home/services.php
@@ -1,1 +1,182 @@
-Services Page!
+<div class="container-fluid m-t-30 m-b-30">
+  <div class="container-80 text-center center-page">
+    <h2>Our Specialized Staffing Services</h2>
+    <p class="m-t-10 p-30">Robert Half’s staffing expertise extends to a wide range of industries. Our specialists connect employers
+      with highly skilled job seekers. With staffing offices across the country,
+      it’s easy to begin your candidate search today.</p>
+  </div>
+
+  <div class="p-t-5 container-80 center-page">
+      <div class="row">
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/demandPlanning.png">
+                  </div>
+                  <h4 class="font-18">Demand Planning</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/supplyPlanning.png">
+                  </div>
+                  <h4 class="font-18">Supply Planning</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/orderFulfilment.png">
+                  </div>
+                  <h4 class="font-18">Order Fulfilment</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+      </div>
+      <!-- end row -->
+
+      <div class="row">
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/logistics.png">
+                  </div>
+                  <h4 class="font-18">Logistics</h4>
+                  <p class="text-muted">We put a lot of effort in design, as it’s the most important ingredient of successful website.Sed ut perspiciatis unde omnis iste natus error sit.</p>
+              </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/manufacturing.png">
+                  </div>
+                  <h4 class="font-18">Manufacturing</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/transportation.png">
+                  </div>
+                  <h4 class="font-18">Transportation</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+      </div>
+      <!-- end row -->
+      <div class="row">
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/procurement.png">
+                  </div>
+                  <h4 class="font-18">Procurement</h4>
+                  <p class="text-muted">We put a lot of effort in design, as it’s the most important ingredient of successful website.Sed ut perspiciatis unde omnis iste natus error sit.</p>
+              </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/certificate.png">
+                  </div>
+                  <h4 class="font-18">Training Certification</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="about-features-box text-center">
+                  <div>
+                      <img src="../include/assets/images/warehousing.png">
+                  </div>
+                  <h4 class="font-18">Warehousing</h4>
+                  <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+          </div>
+      </div>
+  </div>
+  <div class="col-md-8 center-page text-center">
+    <h3 class="m-b-30 m-t-30">Enter your city or ZIP code to find an office near you</h3>
+      <div class="input-group m-t-5">
+          <input type="text" id="" name="" class="form-control form-control-lg" placeholder="Job Title, Skills or Keywords">
+          <span class="input-group-btn">
+              <button type="button" class="btn waves-effect waves-light btn-primary"><i class="fa fa-search m-r-5"></i> Search Jobs</button>
+          </span>
+      </div>
+  </div>
+  <hr class="m-b-30 m-t-30" width="75%">
+  <div class="col-md-8 center-page text-center">
+  <h3 class="m-b-30 m-t-30">More from Teamire</h3>
+  </div>
+  <div class="center-page container-fluid container-80">
+  <div class="col-md-4 services-panel">
+    <img src="../include/assets/images/services-img1.jpg" style="width: 100%;">
+    <div>
+    <h3>
+      <a href="https://www.roberthalf.com/work-with-us/about-robert-half" target="_self">
+        About Us
+      </a>
+    </h3>
+    <p>
+			Find out how we can help you with your hiring and job search needs, access the investor center and learn more about our alliances and community outreach.
+
+    </p>
+    <p class="rh-promos-view-block__read-more">
+      <a href="#" target="_self">
+                  Read More
+              <i class="fa fa-arrow-right"></i></a>
+    </p>
+  </div>
+  </div>
+  <div class="col-md-4 services-panel">
+    <img src="../include/assets/images/services-img2.jpg" style="width: 100%;">
+    <div>
+    <h3>
+      <a href="https://www.roberthalf.com/work-with-us/about-robert-half" target="_self">
+        About Us
+      </a>
+    </h3>
+    <p>
+      Find out how we can help you with your hiring and job search needs, access the investor center and learn more about our alliances and community outreach.
+
+    </p>
+    <p class="rh-promos-view-block__read-more">
+      <a href="#" target="_self">
+                  Read More
+              <i class="fa fa-arrow-right"></i></a>
+    </p>
+  </div>
+  </div>
+  <div class="col-md-4 services-panel">
+    <img src="../include/assets/images/services-img3.jpg" style="width:100%;">
+    <div>
+    <h3>
+      <a href="https://www.roberthalf.com/work-with-us/about-robert-half" target="_self">
+        About Us
+      </a>
+    </h3>
+    <p>
+      Find out how we can help you with your hiring and job search needs, access the investor center and learn more about our alliances and community outreach.
+
+    </p>
+    <p class="rh-promos-view-block__read-more">
+      <a href="#" target="_self">
+                  Read More
+              <i class="fa fa-arrow-right"></i></a>
+    </p>
+  </div>
+  </div>
+
+</div>
+
+
+</div>
