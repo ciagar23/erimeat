5c15e0a3978dda72b7cbecacc1359f9c20b892fe
acknolodgia <torredale1014@gmail.com>
2018-03-08 13:55:10 +0800

    Done updating postedjobs list.

--- company/postedJobs.php
+++ company/postedJobs.php
@@ -16,20 +16,10 @@ function getCount($Id){
   return $employee;
 }
 ?>
-     <div class="row">
-        <div class="col-xs-12">
-            <div class="page-title-box">
-                <h4 class="page-title">Jobs</h4>
-
-                <div class="clearfix"></div>
-            </div>
-        </div>
-    </div>
-<div class="card-box">
   <div class="row">
     <div class="col-sm-12">
       <div class="card-box table-responsive">
-        <h4 class="m-t-0 header-title"><b>List of Jobs</b></h4>
+        <h4 class="page-title">Jobs</h4><br>
         <table id="datatable" class="table table-striped table-bordered">
           <thead>
             <tr>
@@ -69,4 +59,3 @@ function getCount($Id){
       </div>
     </div>
   </div>
-</div>
