7ca0a2ae6fe5df09aa70c651c4297eb9107086ac
acknolodgia <torredale1014@gmail.com>
2018-03-08 16:55:51 +0800

    Updates in jobDetail

--- company/jobDetail.php
+++ company/jobDetail.php
@@ -87,7 +87,7 @@ if (getCount($Id)){
            <td><?=$row->firstName;?> <?=$row->lastName;?></td>
            <td>
              <!--TODO:-->
-             <a href="?view=timesheetDetail&id=<?=$row->Id;?>"  class=" btn btn-success btn-xs tooltips" title="Click To Edit"><span class="fa fa-eye"></span> View Timesheet</a>
+             <a href="?view=timesheetDetail&id=<?=$row->Id;?>&jobId=<?=$row->jobId;?>"  class=" btn btn-success btn-xs tooltips" title="Click To Edit"><span class="fa fa-eye"></span> View Timesheet</a>
            </td>
          </tr>
          <?php
