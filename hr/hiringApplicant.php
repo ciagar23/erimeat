0ebc71da6252371a0e8c5eadcf7576a84711b068
acknolodgia <torredale1014@gmail.com>
2018-03-10 16:15:24 +0800

    Added detail in hiringApplicant

--- hr/hiringApplicant.php
+++ hr/hiringApplicant.php
@@ -2,6 +2,16 @@
 $Id = $_GET['Id'];
 $resume = resume()->get("Id='$Id'");
 $jobId = $_GET['jobId'];
+
+function getJobName($Id){
+  $job = job()->get("Id='$Id'");
+  return $job->position;
+}
+
+function getJobCategory($Id){
+  $jobFunc = job_function()->get("Id='$Id'");
+  return $jobFunc->option;
+}
 ?>
 
 
@@ -12,12 +22,25 @@ $jobId = $_GET['jobId'];
             <h4 class="header-title mt-0 m-b-20">Resume Detail</h4>
             <div class="panel-body">
                 <div class="text-left">
-                    <p class="text-muted font-13"><strong>First Name :</strong>
-                      <span class="m-l-15"><?=$resume->firstName;?></span>
+                    <p class="text-muted font-13"><strong>Applying for :</strong>
+                      <span class="m-l-15"><?=getJobName($resume->jobId);?></span>
                     </p>
-                    <p class="text-muted font-13"><strong>Last Name :</strong>
-                      <span class="m-l-15"><?=$resume->lastName;?></span>
+                    <p class="text-muted font-13"><strong>Candidate Name :</strong>
+                      <span class="m-l-15"><?=$resume->firstName;?> <?=$resume->lastName;?></span>
                     </p>
+                    <p class="text-muted font-13"><strong>Candidate ABN :</strong>
+                      <span class="m-l-15"><?=$resume->abn;?></span>
+                    </p>
+                    <p class="text-muted font-13"><strong>Candidate Email :</strong>
+                      <span class="m-l-15"><?=$resume->email;?></span>
+                    </p>
+                    <p class="text-muted font-13"><strong>Job Category :</strong>
+                      <span class="m-l-15"><?=getJobCategory($resume->jobFunctionId);?></span>
+                    </p>
+                    <p class="text-muted font-13"><strong>Phone Number :</strong>
+                      <span class="m-l-15"><?=$resume->phoneNumber;?></span>
+                    </p>
+                    <p class="text-muted font-13"><strong><a href="../media/<?=$resume->uploadedResume;?>" target="blank_">Click to view resume</a></strong></p>
                 </div>
             </div>
         </div>
