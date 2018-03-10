fb5099bbc357058ce332aa3e1d01309676e705e0
acknolodgia <torredale1014@gmail.com>
2018-03-10 23:36:14 +0800

    Change content of faq

--- home/employerFaq.php
+++ home/employerFaq.php
@@ -2,22 +2,20 @@
 ?>
 
   <h2 class="text-center m-t-30 m-b-30">Employer FAQs</h2>
-  <p class="text-center mb20">Looking for the right candidate? <a href="../home?view=jobseekerFaq" class="text-primary">See job seekers FAQs.</a></p>
+  <p class="text-center mb20">Looking for a job? <a href="../home?view=jobseekerFaq" class="text-primary">See job seekers FAQs.</a></p>
       <div class="clearfix"></div>
       <!--Start 2 panels -->
       <div class="container-80 center-page">
       <div class="row">
         <div class="col-lg-1">&nbsp;</div>
         <div class="col-12 col-lg-4 p-30">
-          <p class="text-center mb20">Looking for the right candidate?</p>
-          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Lorem ipsum</a></b></li></div>
-          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Lorem ipsum</a></b></li></div>
+          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Working with Teamire</a></b></li></div>
+          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Finding Talent</a></b></li></div>
         </div>
         <div class="col-lg-2">&nbsp;</div>
         <div class="col-12 col-lg-4 p-30">
-          <p class="text-center mb20">Looking for a job? We can help.</p>
-          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Lorem ipsum</a></b></li></div>
-          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Lorem ipsum</a></b></li></div>
+          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Timesheet and Payroll</a></b></li></div>
+          <div class="text-center m-b-10 text-primary"><li><b><a href="#">Still need help?</a></b></li></div>
         </div>
         <div class="col-lg-1">&nbsp;</div>
       </div>
@@ -25,27 +23,36 @@
 
     <!-- Start Form Container -->
     <div class="container-80 center-page">
-      <h3 class="form-container">Still have questions?</h3>
+      <h3 class="form-container">Working with Teamire?</h3>
       <div class="container center-page">
         <h4>Q: I’ve never used a staffing agency. How does it work?</h4>
-        <p class="m-b-30">Lorem ipsum dolor sit amet lorem ipsum dolor sit amet  lorem ipsum dolor sit amet
-           lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.</p>
+        <p class="m-b-30">Our staffing specialists take time to get to know your unique hiring needs and
+          ensure they know what you’re looking for. Whether you need one person or an entire team, we can
+          connect you with highly skilled professionals with a wide array of experience. Simply browse our
+          candidates to quickly find the right fit for your open role or tell us your hiring needs by filling
+          out a hiring request form. For personalized assistance, just pick up the phone and give us a call
+          at +61452 364 793.
+        </p>
       </div>
 
-      <h3 class="form-container">Still have questions?</h3>
+      <h3 class="form-container">Finding Talent</h3>
       <div class="container center-page">
         <h4>Q: I’ve never used a staffing agency. How does it work?</h4>
-        <p class="m-b-30">Lorem ipsum dolor sit amet lorem ipsum dolor sit amet  lorem ipsum dolor sit amet
-           lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.</p>
+        <p class="m-b-30">Our staffing professionals have industry-specific expertise in the areas of accounting,
+          finance, technology, legal, administrative, creative and marketing.
+        </p>
       </div>
 
-      <h3 class="form-container">Still have questions?</h3>
+      <h3 class="form-container">Timesheets and Payroll</h3>
       <div class="container center-page">
-        <h4>Q: I’ve never used a staffing agency. How does it work?</h4>
-        <p class="m-b-30">Lorem ipsum dolor sit amet lorem ipsum dolor sit amet  lorem ipsum dolor sit amet
-           lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.</p>
+        <h4>Q: I never received my login information to approve timesheets. Who should I contact?</h4>
+        <p class="m-b-30">First, check your email. You should have received an auto-generated email from
+          onlinetimereporting@roberthalf.com with your login credentials. If you did not receive the email,
+          please contact Customer Service to request new login credentials:
+          <br><br>
+          HR@teamire.com
+          <br><br>
+          +61452 364 793
+        </p>
       </div>
       </div> <!-- End Form Container -->
