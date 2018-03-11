d3a09dba17d2787646bcfe682ce107da3e888b20
designkid <robertcdc21@gmail.com>
2018-03-11 14:52:42 +0800

    Modify employee dashboard (#51)

--- include/navEmployee.php
+++ include/navEmployee.php
@@ -8,34 +8,44 @@
 
     <li>
         <a href="?view=dtr">
-            <i class="mdi mdi-comment-account"></i><span> Current </span>
+            <i class="mdi mdi-clock"></i><span> Current </span>
         </a>
     </li>
 
         <li>
             <a href="?view=timesheets&status=0">
-                <i class="mdi mdi-comment-account"></i><span> Pending </span>
+                <i class="mdi mdi-timer-sand"></i><span> Pending </span>
             </a>
         </li>
 
             <li>
                 <a href="?view=timesheets&status=3">
-                    <i class="mdi mdi-comment-account"></i><span> Approved </span>
+                    <i class="mdi mdi-calendar-check"></i><span> Approved </span>
                 </a>
             </li>
 
 
                 <li>
                     <a href="?view=timesheets&status=2">
-                        <i class="mdi mdi-comment-account"></i><span> Disputes </span>
+                        <i class="mdi mdi-alert-circle-outline"></i><span> Disputes </span>
                     </a>
                 </li>
 
 
                     <li>
                         <a href="?view=timesheets">
-                            <i class="mdi mdi-comment-account"></i><span> Archives </span>
+                            <i class="mdi mdi-archive"></i><span> Archives </span>
                         </a>
+                    </li>
+
+                    <li>
+                        <a href="javascript: void(0);"><i class="fi-briefcase"></i> <span> Lorem </span> <span class="menu-arrow"></span></a>
+                        <ul class="nav-second-level" aria-expanded="false">
+                            <li style="list-style-type: none;" class="p-t-5"><a class="text-white" href="#">Typography</a></li>
+                        </ul>
+                        <ul class="nav-second-level" aria-expanded="false">
+                            <li style="list-style-type: none;" class="p-t-5"><a class="text-white" href="#">Typography</a></li>
+                        </ul>
                     </li>
 
 </ul>
